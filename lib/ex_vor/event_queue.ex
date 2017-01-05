44626c3c6c9e8ec288c402cf520525a240efb185
HongDung <hongdung.nguyen@ruckuswireless.com>
2017-01-05 12:45:51 +0800

    first blood EventQueue

--- lib/ex_vor/event_queue.ex
+++ lib/ex_vor/event_queue.ex
@@ -5,5 +5,32 @@ defmodule ExVor.EventQueue do
     %ExVor.EventQueue{}
   end
 
-  
+  def push(%ExVor.EventQueue{q: q} = event_queue, %ExVor.SiteEvent{} = site_event) do
+    p = site_event_p site_event
+    %{event_queue | q: HeapQueue.push(q,p,site_event)}
+  end
+
+  def push(%ExVor.EventQueue{q: q} = event_queue, %ExVor.CircleEvent{} = circle_event) do
+    p = circle_event_p circle_event
+    %{event_queue | q: HeapQueue.push(q,p,circle_event)}
+  end
+
+  def pop(%ExVor.EventQueue{q: q} = event_queue) do
+    case HeapQueue.pop(q) do
+      {{:value, _, event}, newq} -> {:ok, %{event_queue|q: newq}, event}
+      {:empty, _} -> {:error, q}
+    end
+  end
+
+  def empty?(%ExVor.EventQueue{q: q}) do
+    HeapQueue.empty?(q)
+  end
+
+  defp site_event_p(%ExVor.SiteEvent{x: x, y: y}) do
+    {-y, x}
+  end
+
+  defp circle_event_p(%ExVor.CircleEvent{footer_point: {x, y}}) do
+    {-y, x}
+  end
 end
--- lib/ex_vor/site.ex
+++ lib/ex_vor/site.ex
@@ -1,3 +1,7 @@
 defmodule ExVor.Site do
-  defstruct x: nil, y: nil 
+  defstruct x: nil, y: nil
+
+  def new(x, y) when is_number(x) and is_number(y) do
+    %ExVor.Site{x: x*1.0, y: y*1.0}
+  end
 end
