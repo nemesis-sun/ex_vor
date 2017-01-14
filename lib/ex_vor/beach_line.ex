c899632aa49f1640cbbc5c97a4f1d850fd16f10b
HongDung <hongdung.nguyen@ruckuswireless.com>
2017-01-14 22:30:55 +0800

    BeachLine handle SiteEvent - break the arc

--- lib/ex_vor/beach_line.ex
+++ lib/ex_vor/beach_line.ex
@@ -14,11 +14,12 @@ defmodule ExVor.BeachLine do
 
   def handle_site_event(%ExVor.BeachLine{root: root} = beach_line, 
                         %ExVor.Event.SiteEvent{site: site}) do
-    {node, reversed_path_from_root} = find_covering_arc_node(root, nil, site)
-    break_arc_node(root, node, Enum.reverse(reversed_path_from_root))
+    {node, reversed_path_from_root} = find_covering_arc_node(root, [], site)
+    updated_tree = break_arc_node(root, Enum.reverse(reversed_path_from_root), node, site)
+    %{beach_line | root: updated_tree}
   end
 
-  # terminating case, leaf node encoutered
+  # terminating case, leaf node aka arc encoutered
   defp find_covering_arc_node(%ExVor.BeachLine.Node{left: nil, right: nil} = node,
                               reversed_path_from_root,
                               %ExVor.Geo.Point{}) do
@@ -26,26 +27,45 @@ defmodule ExVor.BeachLine do
   end
 
   # recursive step
-  defp find_covering_arc_node(%ExVor.BeachLine.Node{left: left, right: right, data: data} = node,
+  defp find_covering_arc_node(%ExVor.BeachLine.Node{left: left, right: right, data: data} = _node,
                               reversed_path_from_root,
                               %ExVor.Geo.Point{x: x, y: y} = site) do
 
     %ExVor.BeachLine.BreakPoint{from_site: from_site, to_site: to_site} = data
     sweep_line = ExVor.Geo.HLine.new(y)
 
-    from_y_val = ExVor.Geo.Parabola.new(from_site, sweep_line)
-    |> ExVor.Geo.Parabola.y_value(x)
-
-    to_y_val = ExVor.Geo.Parabola.new(to_site, sweep_line)
-    |> ExVor.Geo.Parabola.y_value(x)
+    [from_y_val, to_y_val] = [from_site, to_site] |> Enum.map(
+      fn(site) ->
+        ExVor.Geo.Parabola.new(site, sweep_line) 
+        |> ExVor.Geo.Parabola.y_value(x)
+      end)
 
     cond do
-      from_y_val > to_y_val -> find_covering_arc_node(left, [:left | reversed_path_from_root], site)
-      true -> find_covering_arc_node(right, [:right | reversed_path_from_root], site)
+      from_y_val > to_y_val -> find_covering_arc_node(right, [:right | reversed_path_from_root], site)
+      true -> find_covering_arc_node(left, [:left | reversed_path_from_root], site)
     end
   end
 
-  defp break_arc_node(root, node, path_from_root) do
-    
+  defp break_arc_node(root, path_from_root, 
+                      %ExVor.BeachLine.Node{data: covering_arc} = _node,
+                      %ExVor.Geo.Point{} = new_site) do
+    # prepare node data
+    %ExVor.BeachLine.Arc{site: covering_site} = covering_arc
+    first_break_point = ExVor.BeachLine.BreakPoint.new(covering_site, new_site)
+    second_break_point = ExVor.BeachLine.BreakPoint.new(new_site, covering_site)
+    new_arc = ExVor.BeachLine.Arc.new(new_site)
+
+    # create and bootstrap nodes
+    covering_arc_node_start = ExVor.BeachLine.Node.new(covering_arc)
+    new_arc_node = ExVor.BeachLine.Node.new(new_arc)
+    covering_arc_node_end = ExVor.BeachLine.Node.new(covering_arc)
+    second_break_point_node = ExVor.BeachLine.Node.new(second_break_point, new_arc_node, covering_arc_node_end)
+    first_break_point_node = ExVor.BeachLine.Node.new(first_break_point, covering_arc_node_start, second_break_point_node)
+
+    # update beach line tree by replacing node with new one
+    case path_from_root do
+      [] -> first_break_point_node
+      _ -> put_in(root, path_from_root, first_break_point_node)
+    end
   end
 end
--- lib/ex_vor/beach_line/node.ex
+++ lib/ex_vor/beach_line/node.ex
@@ -8,4 +8,48 @@ defmodule ExVor.BeachLine.Node do
   def is_leaf?(%ExVor.BeachLine.Node{left: l, right: r}) do
     l == nil && r == nil
   end
+
+  @behaviour Access
+  
+  def fetch(%ExVor.BeachLine.Node{left: l, right: r}, key) do
+    case key do
+      :left -> {:ok, l}
+      :right -> {:ok, r}
+      _ -> :error
+    end
+  end
+
+  def get(%ExVor.BeachLine.Node{} = node, key, default) do
+    case fetch(node, key) do
+      {:ok, value} -> value
+      :error -> default
+    end
+  end
+
+  def get_and_update(%ExVor.BeachLine.Node{} = node, key, cb) do
+    value = fetch(node, key)
+    cb_value = case value do
+      {:ok, value} -> cb.(value)
+      :error -> cb.(nil)
+    end
+
+    case cb_value do
+      {ret, new_value} ->
+        new_node = %{node | key => new_value}
+        {ret, new_node}
+      :pop ->
+        new_node = %{node | key => nil}
+        {value, new_node}
+    end
+  end
+
+  def pop(%ExVor.BeachLine.Node{} = node, key) do
+    case fetch(node, key) do
+      {:ok, value} ->
+        new_node = %{node | key => nil}
+        {value, new_node}
+      :error ->
+        {nil, node}
+    end
+  end
 end
